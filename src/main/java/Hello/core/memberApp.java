a4285dff0095bc39754a7bc896ce76745382bc94
jun <acorn0415@naver.com>
2022-09-29 17:47:11 +0900

    회원 및 주문

---
+++ src/main/java/Hello/core/memberApp.java
@@ -0,0 +1,19 @@
+package Hello.core;
+
+import Hello.core.member.Grade;
+import Hello.core.member.Member;
+import Hello.core.member.MemberService;
+import Hello.core.member.MemberServiceImpl;
+
+public class memberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new memeber = "+member.getName() );
+        System.out.println("find member = "+findMember.getName());
+    }
+}
